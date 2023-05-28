af8c76af8d37e52006cebd982a380a3c844605a8
ZaiPeeKann <81629308+ZaiPeeKann@users.noreply.github.com>
2023-05-28 23:41:12 +0300

    minor updates

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/ZaiPeeKann/puregrade"
 	"github.com/ZaiPeeKann/puregrade/internal/repository"
@@ -70,7 +71,8 @@ func main() {
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
 
-	if err = srv.Shutdown(context.Background()); err != nil {
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	if err = srv.Shutdown(ctx); err != nil {
 		log.Printf("Error occured on server shutting down: %s", err.Error())
 	}
 	if err = db.Close(); err != nil {
